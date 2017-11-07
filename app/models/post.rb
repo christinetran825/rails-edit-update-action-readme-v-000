22d24a4de785fbff64290050d3cf0e2450d967f6
Christine Tran <christinetran825@gmail.com>
2017-11-07 02:41:44 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,12 @@
 class Post < ActiveRecord::Base
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(title: params[:title], description: params[:description])
+    redirect_to post_path(@post)
+  end
 end
